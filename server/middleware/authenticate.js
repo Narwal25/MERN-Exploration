861d17941a7ba4a2644b6d00962e0dac178cfebb
Narwal <ritiknarwal588@gmail.com>
2024-02-11 20:57:18 +0530

    Second Commit

--- server/middleware/authenticate.js
+++ server/middleware/authenticate.js
@@ -1,23 +1,28 @@
-const jwt  = require('jsonwebtoken');
+const jwt = require('jsonwebtoken');
 const User = require("../model/userSchema");
 
 
-const  Authenticate = async (req, res, next) =>{
-    try{
+const Authenticate = async (req, res, next) => {
+
+    try {
 
         const token = req.cookies.jwtoken;
+
+        if (!token) {
+            return res.status(400).json({ message: "Require login"});
+        }
         const veryifyToken = jwt.verify(token, process.env.SECRET_KEY);
-        const rootUser = await User.findOne({_id: verifyToken._id, "tokens.token": token});
-        
-        if (!rootUser) { throw new Error ("Couldn't find User")}
+        const rootUser = await User.findOne({ _id: verifyToken._id, "tokens.token": token });
+
+        if (!rootUser) { throw new Error("Couldn't find User") }
 
         req.token = token;
         req.rootUser = rootUser;
         req.userID = rootUser._id;
 
 
-    }catch(err){
-        res.status(401).send('Unauthorized');
+    } catch (err) {
+        res.status(401).json({ message: 'Unauthorized' });
         console.log(err);
     }
 }
